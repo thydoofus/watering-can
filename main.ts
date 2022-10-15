b6c013c546a23a46203ca593f16485f293b7106f
thydoofus <77323809+thydoofus@users.noreply.github.com>
2022-10-16 01:34:48 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+let X_tilt = Math.atan2(input.acceleration(Dimension.X), 0)
 basic.forever(function () {
     basic.showNumber(Math.round(57.2957795131 * Math.atan2(input.acceleration(Dimension.X), 0)))
+    if (X_tilt <= -600) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
 })
