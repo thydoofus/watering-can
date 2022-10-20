fddf6ef11aff4709b149de860eea60612ee3bd8b
thydoofus <77323809+thydoofus@users.noreply.github.com>
2022-10-20 22:55:03 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ basic.forever(function () {
     // Activates when watering can raise past a certain height
     if (Math.round(input.acceleration(Dimension.X)) < -700 && Math.round(input.acceleration(Dimension.Z)) > 5000) {
         basic.showIcon(IconNames.Happy)
+        music.playMelody("C E G C5 C5 G E C ", 120)
     } else {
         basic.showIcon(IconNames.Sad)
     }
