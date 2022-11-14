9f8b56f252bc34395bca62f38307351dab5d33a8
thydoofus <77323809+thydoofus@users.noreply.github.com>
2022-11-14 13:29:53 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 let X_tilt = Math.round(input.acceleration(Dimension.X))
 basic.forever(function () {
     // Activates when watering can tilt pass certain angle
-    // Activates when watering can raise past a certain height
-    if (Math.round(input.acceleration(Dimension.X)) < -700 && Math.round(input.acceleration(Dimension.Z)) > 1000) {
+    if (Math.round(input.acceleration(Dimension.X)) < -700) {
         basic.showIcon(IconNames.Happy)
         music.playMelody("C E G C5 C5 G E C ", 120)
     } else {
